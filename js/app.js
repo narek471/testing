27368687d7066f52cd9a3ac4dd924eda10e2c425
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2023-05-26 23:13:29 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,5 +4,12 @@ window.addEventListener('scroll', e => {
     document.documentElement.style.setProperty('--shiftY', `${this.scrollY}px`)
 })
 
+// document.querySelectorAll('.home-link').forEach(button => {
+//     button.addEventListener('click', (e)=> {
+//         e.preventDefault();
+//         history.back();
+//     })
+// })
+
 
 
