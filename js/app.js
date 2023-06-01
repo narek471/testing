43dfdb861bf55ccbd7fdac897a6f4d2acf7840d1
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2023-06-02 02:22:40 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,36 +1,7 @@
-AOS.init();
-
-// Parallax
-
-window.addEventListener('scroll', e => {
-    document.documentElement.style.setProperty('--shiftY', `${this.scrollY}px`)
+$('.header__banner__carousel').owlCarousel({
+    loop:true,
+    margin:150,
+    nav:false,
+    items:1,
+    autoplay:false
 })
-
-// Back to home page
-
-// document.querySelectorAll('.home-link').forEach(button => {
-//     button.addEventListener('click', (e)=> {
-//         e.preventDefault();
-//         history.back();
-//     })
-// })
-
-// On - Off Map
-
-// const mapImg = document.querySelector('.map__main__right');
-// const map = document.querySelector('.google-map');
-// const mapButton = document.querySelector('.map__button');
-//
-// mapButton.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     if(map.style.display === 'none') {
-//         mapImg.style.display = 'none';
-//         map.style.display = 'block';
-//     }else {
-//         mapImg.style.display = 'block';
-//         map.style.display = 'none';
-//     }
-// })
-
-
-
