2ab6261f9eda59a099c35958a817f3c6fecb1a33
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2023-06-07 18:22:24 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
-$('.header__banner__carousel').owlCarousel({
-    loop:true,
-    margin:150,
-    nav:false,
-    items:1,
-    autoplay:false
-})
+const button = document.querySelector('.header__mobile__button');
+const mobileMenu = document.querySelector('.mobile__menu');
+const menuItems = document.querySelectorAll('.mobile__menu__link');
+const spans = document.querySelectorAll('.header__mobile__span');
 
-$('.models__carousel').owlCarousel({
-    autoWidth: true,
-    loop:false,
-    margin:30,
-    nav:false,
-    dots: false,
-    items: 1,
-    autoplay:false,
-})
+if(button && mobileMenu) {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        mobileMenu.classList.toggle('active');
+        if(mobileMenu.classList.contains('active')) {
+            document.body.style.overflowY = 'hidden';
+            spans.forEach(e => {
+                e.classList.toggle('active');
+            })
+        } else {
+            document.body.style.overflowY = 'auto';
+            spans.forEach(e => {
+                e.classList.toggle('active');
+            })
+        }
+    })
+}
+
+if(menuItems) {
+    menuItems.forEach(element => {
+        element.addEventListener('click', (e) => {
+            e.preventDefault();
+            mobileMenu.classList.toggle('active');
+            document.body.style.overflowY = 'auto';
+        })
+    })
+}
